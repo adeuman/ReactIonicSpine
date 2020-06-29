b8a1566d5dade9f051723c0bb3690ae7a62dc47f
Alex Deuman <alex@cockpitmobile.com>
2020-06-29 08:22:10 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import '@ionic/react/css/core.css';
 import './App.css';
+import { IonApp, IonButton, IonRouterOutlet, IonPage } from '@ionic/react';
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import { IonReactRouter } from "@ionic/react-router";
+import Home from './Home';
+import Route from "react-router-dom/es/Route";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <IonApp>
+            <IonReactRouter>
+                <IonRouterOutlet>
+                    <Route path="/" component={Home} exact={true}/>
+                </IonRouterOutlet>
+            </IonReactRouter>
+        </IonApp>
+    );
 }
 
 export default App;
