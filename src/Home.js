b4cd8e1fe85842d5cbc9540172543ff071c95471
WestonN <weston425@gmail.com>
2020-07-01 08:38:28 -0500

    aight

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,10 @@ import React, {useEffect} from 'react';
 import { init, stopAnimationRender } from './webgl';
 import { ScreenOrientation } from "@ionic-native/screen-orientation";
 
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture/ngx';
+
 function Home() {
+
     useEffect(async () => {
         await ScreenOrientation.lock(ScreenOrientation.ORIENTATIONS.LANDSCAPE);
     });
@@ -11,12 +14,23 @@ function Home() {
         stopAnimationRender();
     };
 
+    const captureAudio = () => {
+        let mediaCapture = new MediaCapture();
+
+        mediaCapture.captureAudio(1)
+          .then(
+            (data) => {console.log(data); console.log("whyyyyyyyyyyy")}
+          ).catch(
+            (err) => console.log("I'm not sure which error I should look at!"+err)
+          );
+    };
+
     return (
             <div className="App">
                 <header className="App-header">
                     <p style={{color: "black"}}>Test Ionic React App for Amira</p>
                     <div style={{flexDirection: 'column'}}>
-                        <IonButton onClick={init}>Start</IonButton>
+                        <IonButton onClick={() => {init();captureAudio(); }}>Start</IonButton>
                         <IonButton onClick={clearCanvas}>Stop</IonButton>
                     </div>
                     <canvas id="canvas" style={{width: 200, height: 200}}/>
